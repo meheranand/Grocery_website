a90a7a4fc10d9016fc6b21cd0694ee0646d12603
dma <mehernandu642@gmail.com>
2025-07-08 11:03:28 +0530

    updated origins again

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port=4000
 
 const allowedorigins = [
   "http://localhost:5173",
-  "https://grocery-website-anand-phi.vercel.app",
+  "https://grocery-website-anand.vercel.app",
 ];
 
 //middleware
